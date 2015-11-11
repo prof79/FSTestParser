b17c72cf5c515b90d0f6b96dcfc163671ff03ab6
Markus M. Egger <markus@markusegger.at>
2015-11-12 00:42:24 +0100

    Goodl progress towards a functional lexer/parser.

--- TestParser/TestParser.fs
+++ TestParser/TestParser.fs
@@ -1,8 +1,14 @@
-﻿module TestParser
+﻿(* KUDOS to alf42red/Armin Heller https://www.youtube.com/user/alf42red *)
+
+module TestParser
 
 open System
 
-type MyParser<'a, 'b> = 'a list -> ('b * 'a list) option
+(* Parser *)
+
+type MyParserResult<'a, 'b> = ('b * 'a list) option
+
+type MyParser<'a, 'b> = 'a list -> MyParserResult<'a, 'b>
 
 let explode (str : string) =
     str.ToCharArray()
@@ -16,13 +22,23 @@ let is_digit = Char.IsDigit
 
 let is_letter = Char.IsLetter
 
-let is_space c =
-    c = ' ' || c = '\r' || c = '\n' || c = '\t' || c = '\v'
+let is_space ch =
+    ch = ' ' || ch = '\r' || ch = '\n' || ch = '\t' || ch = '\v'
 
-let is_punct c =
-    not <| is_digit c &&
-    not <| is_letter c &&
-    not <| is_space c
+let is_punct ch =
+    not <| is_digit ch &&
+    not <| is_letter ch &&
+    not <| is_space ch
+
+let digitchar2int ch =
+    (Char.GetNumericValue(ch) |> int) - (Char.GetNumericValue('0') |> int)
+
+(* Checks whether xs start with ss *)
+let rec starts_with xs ss =
+    match xs, ss with
+    | _, []  -> true
+    | [], _  -> false
+    | x :: xs, s :: ss -> if x = s then starts_with xs ss else false
 
 let check pred : MyParser<'a, 'a> = function
     | x :: xs when pred x -> Some (x, xs)
@@ -37,7 +53,7 @@ let pdigit  = check is_digit
 let pspace  = check is_space
 let ppunct  = check is_punct
 
-let expect c cs = check ((=) c) cs
+let expect ch = check ((=) ch)
 
 let (|||) (parser1 : MyParser<'a, 'b>) (parser2 : MyParser<'a, 'b>) : MyParser<'a, 'b> =
     fun xs ->
@@ -45,7 +61,7 @@ let (|||) (parser1 : MyParser<'a, 'b>) (parser2 : MyParser<'a, 'b>) : MyParser<'
         | Some res -> Some res
         | None -> parser2 xs
 
-let (>>>) (parser : MyParser<'a, 'b>) (f : 'b -> 'c) xs =
+let (>>>) (parser : MyParser<'a, 'b>) (f : 'b -> 'c) xs : MyParserResult<'a, 'c> =
     match parser xs with
     | Some (x, xs) -> Some (f x, xs)
     | None -> None
@@ -58,7 +74,7 @@ let (<&>) (parser1 : MyParser<'a, 'b>) (parser2 : MyParser<'a, 'c>) : MyParser<'
                              | None -> None
         | None -> None
 
-let empty xs = Some ((), xs)
+let empty xs : MyParserResult<'a, unit> = Some ((), xs)
 
 let rec repeat parser xs =
     xs |> ((parser <&> (repeat parser) >>> (fun (res1, res2) -> res1 :: res2)) |||
@@ -67,28 +83,50 @@ let rec repeat parser xs =
 let repeat1 parser xs =
     xs |> (parser <&> (repeat parser) >>> (fun (res1, res2) -> res1 :: res2))
 
+(* Spaces are optional ("repeat" not "repeat1") *)
+let pspaces = repeat pspace
+
+
+(* Tokenizer/Lexer *)
+
 type MyToken =
     | Id of string
     | Keyword of string
     | Operator of string
     | Integer of int
 
-let pspaces cs = repeat pspace cs
+type MyTokenizer = MyParser<char, MyToken>
 
-let tokops cs = cs |> ((repeat1 ppunct) >>> fun cs -> Operator (implode cs))
+type MyLexer = string list -> string -> MyToken list
 
-let tokinteger cs =
-    match cs |> (repeat1 pdigit) with
-    | Some (cs, rest) ->
-            let intval = cs
-                         |> List.map (fun c ->
-                                (Char.GetNumericValue(c) |> int) -
-                                (Char.GetNumericValue('0') |> int))
-                         |> List.fold (fun state i -> state * 10 + i) 0
-            Some (Integer intval, rest)
+let tokops = (repeat1 ppunct) >>> (implode >> Operator)
 
-    | None -> None
+let tokinteger =
+    (repeat1 pdigit) >>> fun ds -> ds
+                                   |> List.map digitchar2int
+                                   |> List.fold (fun state i -> state * 10 + i) 0
+                                   |> Integer
 
+let tokidentifier (keywords : string list) =
+    (pletter <&> repeat (pletter ||| pdigit ||| expect '_'))
+        >>> fun (res1, res2) -> let ident = res1 :: res2
+                                match keywords
+                                      |> List.tryFind (fun kw -> kw = implode ident)
+                                      with
+                                      | Some (kw) -> Keyword kw
+                                      | None -> Id <| implode ident
+
+let lex keywords str =
+    explode str
+    (* ignore spaces *)
+    |> (repeat (pspaces <&> (tokidentifier keywords |||
+                             tokops |||
+                             tokinteger)
+            >>> snd))
+
+
+
+(* Dummy *)
 
 type TestClass() = 
     member this.Hello = "F#"
